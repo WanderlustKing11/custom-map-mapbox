bbfedcacc5b57c538b7e508a9c8bdf38e1fa98ae
WanderlustKing11 <WanderlustKing11@gmail.com>
2024-02-15 12:34:55 -0500

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,19 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
+          Here&apos;s a custom interactive map
+          {/* <code className={styles.code}>app/page.tsx</code> */}
         </p>
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://www.mapbox.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
             By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/mapbox_logo.png"
+              alt="Mapbox Logo"
               className={styles.vercelLogo}
               width={100}
               height={24}
@@ -28,7 +28,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className={styles.center}>
+      {/* <div className={styles.center}>
         <Image
           className={styles.logo}
           src="/next.svg"
@@ -89,7 +89,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
