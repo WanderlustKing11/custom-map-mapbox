c4dc68920d1d5a31346f6c79916cb359f4b1a9f1
WanderlustKing11 <WanderlustKing11@gmail.com>
2024-02-16 19:57:48 -0500

    Update map size

--- components/MapComponent.client.tsx
+++ components/MapComponent.client.tsx
@@ -9,20 +9,34 @@ if (typeof process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN === 'string') {
   console.error('Mapbox acces token is not set!');
 }  
 
+// navigator.geolocation.getCurrentPosition(successLocation, errorLocation, { 
+//   enableHighAccuracy: true 
+// })
+
+// function successLocation(position: any) {
+//   console.log(position)
+//   return [position.coords.longitude, position.coords.latitude];
+// }
+
+// function errorLocation() {
+
+// }
+
+
 const MapComponent = () => {
   useEffect(() => {
     // Run once the component mounts
     const map = new mapboxgl.Map({
         container: 'map', // container ID should match the ID of the div below
         style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: [-74.5, 40], // starting position [lng, lat]
+        center: [-74.5, 40], // successLocation(), // starting position [lng, lat]
         zoom: 9, // starting zoom
     });
 
     return () => map.remove();    
     }, []);
   
-  return <div id='map' style={{ height: '400px', width: '100%' }}></div>;
+  return <div id='map' style={{ height: '100vh', width: '90vw' }}></div>;
 };
 
 export default MapComponent;
